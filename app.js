f19766c7b45de4d8b784be65fd659e90abfe7b9d
eugeneleychenko <nycspicebo@gmail.com>
2020-10-12 17:01:39 -0400

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+/*
+GAME FUNCTION:
+  - Player must guess a number between a min and max
+  - Player gets a certain amount of guesses
+  - Ntoify player of guesses remaing
+  - Notify the player of the correct answer if lose
+  - Let player choose to play again
+*/
+
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const game = document.getElementById("game"),
+      minNum = document.querySelector(".min-num"),
+      maxNum = document.querySelector(".max-num"),
+      guessBtn = document.querySelector(".guess-btn"),
+
