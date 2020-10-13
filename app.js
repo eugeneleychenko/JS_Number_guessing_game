7d7a9d0f1ca616da8d178329b9a498caaa31b84b
eugeneleychenko <nycspicebo@gmail.com>
2020-10-13 16:43:57 -0400

    finished

--- app.js
+++ app.js
@@ -9,11 +9,71 @@ GAME FUNCTION:
 
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getWinningNum(min, max),
     guessesLeft = 3;
 
 const game = document.getElementById("game"),
       minNum = document.querySelector(".min-num"),
       maxNum = document.querySelector(".max-num"),
-      guessBtn = document.querySelector(".guess-btn"),
+      guessBtn = document.querySelector("#guess-btn"),
+      guessInput = document.querySelector("#guess-input"),
+      message = document.querySelector(".message");
 
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener("click", function(){
+  let guess = parseInt(guessInput.value);
+  console.log(guess)
+
+  if(isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red")
+  
+
+  } if(guess === winningNum){
+      gameOver(true, `${winningNum} is correct. You win!`)
+  } else {
+    guessesLeft -= 1;
+    
+    if(guessesLeft < 1) {
+      gameOver(false, `You're all out of guesses. The correct numbers was ${winningNum}`)
+      guessBtn.value = "Play Again";
+      guessBtn.addEventListener("click", function(e){
+        location.reload();
+      })
+
+      // guessInput.disabled = true;
+      console.log("random #: ", winningNum);
+      console.log("guessesLeft: ", guessesLeft)
+    } else {
+      guessInput.disabled = false;
+      guessInput.style.borderColor = "red"
+      guessInput.value = "";
+      setMessage( `${guess} is incorrect. You have ${guessesLeft} left!`, "red");
+      console.log("guessesLeft: ", guessesLeft);
+      console.log("random #: ", winningNum);
+    }
+
+  }
+  function gameOver(won, msg) {
+    let color;
+    won === true ? color = "green" : color = "red";
+    
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    
+    setMessage(msg);
+  }
+
+})
+
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
+
+function getWinningNum(min, max){
+  return winningNum = Math.floor(Math.random()*(max-min+1)+min);
+  
+}
